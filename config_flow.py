a83a44d392312f0bab8b04feba367f37dd7c1103
Dan4Jer <jdanoffre@gmail.com>
2025-11-16 10:58:09 +0100

    bug fixing on config_flow

--- config_flow.py
+++ config_flow.py
@@ -6,9 +6,19 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
-from .const import DOMAIN, CONF_API_USER, CONF_API_SECRET, CONF_API_HOST, DEFAULT_API_HOST
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .const import DOMAIN, CONF_API_USER, CONF_API_SECRET, CONF_API_HOST, DEFAULT_API_HOST, DEFAULT_API_USER, DEFAULT_API_SECRET
+from .eedomus_client import EedomusClient
 
 _LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_API_HOST, default=DEFAULT_API_HOST): str,
+        vol.Required(CONF_API_USER, default=DEFAULT_API_USER): str,
+        vol.Required(CONF_API_SECRET, default=DEFAULT_API_SECRET): str,
+    }
+)
 
 class EedomusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for eedomus."""
@@ -20,19 +30,42 @@ class EedomusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is None:
             # Use vol.Schema instead of the deprecated CONFIG_SCHEMA
             return self.async_show_form(
-                step_id="user",
-                data_schema=vol.Schema({
-                    vol.Required(CONF_API_HOST, default=DEFAULT_API_HOST): str,
-                    vol.Required(CONF_API_USER): str,
-                    vol.Required(CONF_API_SECRET): str,
-                })
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
             )
 
         _LOGGER.debug("Config flow user input: %s", user_input)
 
         # Here you would typically validate the connection
-        # For now we'll just create the entry
-        return self.async_create_entry(
-            title="eedomus",
-            data=user_input,
+
+        errors = {}
+
+        try:
+            info = await self.validate_input(user_input)
+        except vol.Invalid as err:
+            errors["base"] = str(err)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return self.async_create_entry(title=info["title"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
+    
+    async def validate_input(self, data: dict[str, Any]) -> dict[str, Any]:
+        """Validate the user input allows us to connect."""
+        session = async_get_clientsession(self.hass)
+        client = EedomusClient(
+            session=session,
+            api_host=data[CONF_API_HOST],  # Passer l'hôte au client
+            api_user=data[CONF_API_USER],
+            api_secret=data[CONF_API_SECRET],
+        )
+        _LOGGER.debug("Config flow validate input: %s", client)
+        # Test the connection by trying to fetch peripheral list
+        rdata = await client.auth_test()
+        if not rdata or rdata.get("success", 0) != 1:
+            raise vol.Invalid("Cannot connect to eedomus API - please check your credentials and host")
+
+        return {"title": f"Eedomus ({data[CONF_API_HOST]})"}
